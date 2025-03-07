b9a1247c942bfc7357262b8fa9e8518506ec1b31
Miheer Gokhale <miheergokhale24@gmail.com>
2025-03-07 14:05:35 +0530

    lastname solve

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -6,7 +6,7 @@ export const POST = async (req:NextRequest)=>{
 
     const emailAddress = data.email_addresses[0].email_address;
     const firstName = data.first_name;
-    const lastName = data.last_name;
+    const lastName = data.last_name || "prototype";
     const imageUrl = data.image_url;
     const id = data.id;
     
