cd4dbfa7ec30e2a489a3264d6bebb2fcf0bbec84
Miheer Gokhale <miheergokhale24@gmail.com>
2025-03-07 13:26:50 +0530

    initial commit for clerk

---
+++ src/app/api/clerk/webhook/route.ts
@@ -0,0 +1,26 @@
+import { db } from "@/server/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req:NextRequest)=>{
+    const {data} = await req.json();
+
+    const emailAddress = data.email_addresses[0].email_address;
+    const firstName = data.first_name;
+    const lastName = data.last_name;
+    const imageUrl = data.image_url;
+    const id = data.id;
+    
+    await db.user.create({
+        data:{
+            id,
+            emailAddress,
+            firstName,
+            lastName,
+            imageUrl
+        }
+    })
+
+    console.log("done");
+
+    return new NextResponse("Webhook received", {status:200});
+}
